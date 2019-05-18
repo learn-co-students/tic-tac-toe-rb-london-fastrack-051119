273771b3614e54a0dc9f50b10e43862df1a72fe9
Eliot Howes <ejhowes@gmail.com>
2019-05-18 08:19:44 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,103 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
+
+def display_board(board)
+  puts " #{board[0]} " "|" " #{board[1]} " "|" " #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} " "|" " #{board[4]} " "|" " #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} " "|" " #{board[7]} " "|" " #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, position, x_o)
+  board[position] = x_o
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && !(position_taken?(board, index))
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, "X")
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  turn = 0
+  board.each do |position|
+    if position != " "
+      turn += 1
+    end
+  end
+  return turn
+end
+
+def current_player(board)
+  if turn_count(board) % 2 != 0
+     return "O"
+  else
+     return "X"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |winning_positions|
+    if winning_positions.all? {|game_square| board[game_square] == "X"} || winning_positions.all? {|game_square| board[game_square] == "O"}
+       return winning_positions
+    end
+  end
+   false
+end
+
+def full?(board)
+  board.none? {|game_square| game_square == " "}
+end
+
+def draw?(board)
+  if full?(board) && won?(board)
+    false
+  elsif !full?(board) && !won?(board)
+    false
+  elsif full?(board) && !won?(board)
+    true
+  end
+end
+
+def over?(board)
+  if full?(board) || draw?(board) || won?(board)
+    return true
+  end
+end
+
+def winner(board)
+  if !won?(board)
+      return nil
+  elsif board[won?(board)[0]] && board[won?(board)[1]] && board[won?(board)[2]] == "X"
+      return "X"
+  elsif board[won?(board)[0]] && board[won?(board)[1]] && board[won?(board)[2]] == "O"
+      return "O"
+  end
+end
